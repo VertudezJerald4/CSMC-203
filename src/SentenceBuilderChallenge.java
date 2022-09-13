7c794ee1919eb2b7344992bbf458c0ece15750cc
VertudezJerald4 <112453807+VertudezJerald4@users.noreply.github.com>
2022-09-13 02:29:54 -0700

    Add files via upload

---
+++ src/SentenceBuilderChallenge.java
@@ -0,0 +1,17 @@
+
+public class SentenceBuilderChallenge {
+
+	public static void main(String[] args) {
+		String name = "Jerald Vertudez";
+		int age = 20;
+		double gpa = 2.25;
+		String bldtype = "B";
+		
+		System.out.println(" Hi my name is " + name);
+		System.out.println(" I am " + age + "years old");
+		System.out.println(" My GPA is " + gpa);
+		System.out.println(" My blood type is " + bldtype);
+
+	}
+
+}
